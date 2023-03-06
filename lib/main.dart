1c92c9d07bc7212a329a6279b91754cb5f196e65
unknown <gangwarnisha21@gmail.com>
2023-03-06 16:37:21 +0530

    day 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/Home_Page.dart';
 
 //import 'home_page.dart';
 
 void main() {
   runApp( MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    // int days=30;
     //String name="codepur";
     return MaterialApp(
-      home:Material(
-        child:Center(
-         child:Container(
-         child:Text("welcom to rkgit"),
-        ),
-       
-        ), 
-    ),
+      home:HomePage()
     );
   } 
 }
